50fac8d120131e1e6c1836e62dde6de1d3cf90ea
Andres Luque <andresl@biccofarms.com>
2020-04-01 23:36:42 -0500

    Ejemplo de uso de fixtures de PHPUnit

--- tests/OperationsTests.php
+++ tests/OperationsTests.php
@@ -13,13 +13,33 @@ class OperationsTests extends TestCase {
     6 => 720
   ];
 
+  protected static $db;
+
+  public static function setUpBeforeClass() : void {
+    self::$db = "Conectado";
+  }
+
+  public static function tearDownAfterClass() : void {
+    self::$db = "";
+    echo self::$db . "!";
+  }
+
   public function testFactorialNumber() : void {
+    echo self::$db;
     $operation = new Operations();
     foreach($this->_expected as $number => $expected){
       $factorial = $operation->factorial($number);
       $this->assertIsInt($factorial);
       $this->assertEquals($expected, $factorial, "El factorial de $number debe ser $expected y no $factorial!");
     }
+  }
+
+  public function testOne() : void {
+    $this->assertTrue(true);
+  }
+
+  public function testTwo() : void {
+    $this->assertTrue(true);
   }
 
 
